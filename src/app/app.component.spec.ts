8a73449f7f243abe1ee43d79b2fc41f990bf69d3
JaremGallegos <139504059+JaremGallegos@users.noreply.github.com>
2024-09-16 20:56:36 -0500

    PearsonSys 1.0.1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button'
 
 describe('AppComponent', () => {
   beforeEach(async () => {
