3416a1c8c6861bb8dc116b9a7811bd2c2a2df64f
JaremGallegos <139504059+JaremGallegos@users.noreply.github.com>
2024-10-09 04:41:03 -0500

    Pearson 1.0.4

---
+++ src/app/landlord/model/listing-vo.model.ts
@@ -0,0 +1,27 @@
+export interface HuespedVO {
+    value: number
+}
+
+export interface CuartoVO {
+    value: number
+}
+
+export interface CamaVO {
+    value: number
+}
+
+export interface BanoVO {
+    value: number
+}
+
+export interface TituloVO {
+    value: string
+}
+
+export interface DescripcionVO {
+    value: string
+}
+
+export interface PrecioVO {
+    value: number
+}
---
+++ src/app/landlord/model/listing.model.ts
@@ -0,0 +1,59 @@
+import { CategoryName } from "../../layout/navbar/category/category.model";
+import {NewListingPicture} from "./picture.model";
+import { BanoVO, CamaVO, CuartoVO, DescripcionVO, HuespedVO, PrecioVO, TituloVO } from "./listing-vo.model";
+
+
+export interface NewListingInfo  {
+    guests: HuespedVO,
+    bedrooms: CuartoVO,
+    beds: CamaVO,
+    baths: BanoVO
+}
+
+export interface NewListing {
+    category: CategoryName,
+    location: string,
+    infos: NewListingInfo,
+    pictures: Array<NewListingPicture>,
+    description: Description,
+    price: PrecioVO
+}
+
+export interface Description {
+    title: TituloVO,
+    description: DescripcionVO
+}
+
+export interface CreatedListing {
+    publicId: string
+}
+
+export interface DisplayPicture {
+    file?: string,
+    fileContentType?: string,
+    isCover?: boolean
+}
+
+export interface CardListing {
+    price: PrecioVO,
+    location: string,
+    cover: DisplayPicture,
+    bookingCategory: CategoryName,
+    publicId: string,
+    loading: boolean
+}
+
+export interface Listing {
+    description: Description,
+    pictures: Array<DisplayPicture>,
+    infos: NewListingInfo,
+    price: PrecioVO,
+    category: CategoryName,
+    location: string,
+    landlord: LandlordListing
+}
+
+export interface LandlordListing {
+    firstname: string,
+    imageUrl: string
+}
